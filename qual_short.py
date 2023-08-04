b512c95d0e93a06319cc6bfb25eed34f11319d43
Deepali Kundnani <deepali.kundnani@gmail.com>
2023-08-04 13:17:01 -0400

    AGS update
    more complete

---
+++ qual_short.py
@@ -0,0 +1,93 @@
+#!usr/bin/env python3
+#Author: Deepali L. Kundnani
+#Institution: Georgia Institute of Technology (Georgia Tech)
+#This script is to be ran from the results folder of Ribosemap run to output unique read percentage
+#Activate the conda enviroment for Ribosemap before running this script(or any that provides samtools and bedtools)
+
+import subprocess
+import pandas as pd
+import os
+import glob
+import argparse
+parser = argparse.ArgumentParser(description='Quality statitics of ribosemap runs for post Alignment, Coordinate, Composition modules')
+
+parser.add_argument("-f", "--file", required=True, default='/storage/home/hcoda1/5/dkundnani3/p-fstorici3-0/rich_project_bio-storici/AGS/files', help="tab separated file with first column with library id mentioned in Ribosemap, and additional sample names if to be attached int the final file.")
+parser.add_argument("-r", "--referencefasta" ,required=True, default='/storage/home/hcoda1/5/dkundnani3/p-fstorici3-0/rich_project_bio-storici/reference/sacCer2/sacCer2.fa', help="Specify the fasta file for reference of the sequenced Libraries. Make sure you have Ribosemap environment activated or have bedtools available to be used") 
+args= parser.parse_args()
+
+sample_names=pd.read_csv(args.file,sep='\t', header=None)
+
+print("Sample information provided")
+print(sample_names)
+
+df = pd.DataFrame(columns=['Library','Counts_All','Counts_Nuclear','Counts_Mito','Comp_nucl_rA','Comp_nucl_rC','Comp_nucl_rG','Comp_nucl_rU','Freq_nucl_rA','Freq_nucl_rC','Freq_nucl_rG','Freq_nucl_rU','Comp_mito_rA','Comp_mito_rC','Comp_mito_rG','Comp_mito_rU','Freq_mito_rA','Freq_mito_rC','Freq_mito_rG','Freq_mito_rU','pos>1_nucleus/totpos', 'pos>1_mito/totpos'], dtype=object)
+
+#subprocess.call("conda activate conda_env", shell=True, universal_newlines=True)
+for lib in sample_names[0]:
+    file=glob.glob("*{}*".format(lib))[0]
+    print("Analyzing "+file)
+    #Parameter SetI:Processing Statitics: Raw reads from fastq files, Aignment rate, percent reads barcoded, unique read percent
+    
+    #Paramter Set II: Counts, Composition and Frequency in Nucleus and Mitochondria
+    '''
+    chrX_reads=subprocess.check_output("grep chrX "+file+"/coordinate30/"+file+".bed | wc -l", shell=True, universal_newlines=True)
+    #parameter 9: Reads in Chr Y
+    chrY_reads=subprocess.check_output("grep chrY "+file+"/coordinate30/"+file+".bed | wc -l", shell=True, universal_newlines=True)
+    
+    
+    #Paramter set 10:  Perc of rN(A,C,G,U) in mitochondria and nucleus.
+    subprocess.call("bedtools getfasta -s -bedOut -fi "+args.referencefasta+" -bed "+file+"/coordinate30/"+file+"-chrM.bed > "+file+"/coordinate30/"+file+"-chrM_seq.bed",shell=True, universal_newlines=True)
+    subprocess.call("bedtools getfasta -s -bedOut -fi "+args.referencefasta+" -bed "+file+"/coordinate30/"+file+"-chromosomes.bed > "+file+"/coordinate30/"+file+"-chromosomes_seq.bed",shell=True, universal_newlines=True)
+    
+    for ribo in ['A','C','G','T','N']:
+         prefNTP='comp_nucl'+ribo
+         temp=subprocess.check_output("grep -v chrM "+file+"/coordinate30/"+file+"-chrM_seq.bed | grep -i '\s"+ribo+"$'| wc -l", shell=True, universal_newlines=True)
+         vars()[prefNTP]=int(temp.strip('\n'))
+    for ribo in ['A','C','G','T','N']:
+         prefNTP='comp_mito'+ribo
+         temp=subprocess.check_output("grep chrM "+file+"/coordinate30/"+file+"-chromosomes_seq.bed | grep -i '\s"+ribo+"$'| wc -l", shell=True, universal_newlines=True)
+         vars()[prefNTP]=int(temp.strip('\n'))
+    '''
+    mito_total=int(subprocess.check_output("cat "+file+"/coordinate30/"+file+"-chrM.bed | wc -l", shell=True, universal_newlines=True))
+    nucl_total=int(subprocess.check_output("cat "+file+"/coordinate30/"+file+"-chromosomes.bed | wc -l", shell=True, universal_newlines=True))
+    total_ribos=mito_total+nucl_total
+
+    comp_mito=subprocess.check_output("cut -f2 "+file+"/composition30/"+file+"-chrM.counts.txt", shell=True, universal_newlines=True)
+    comp_mito=comp_mito.split('\n')
+    comp_mito=[int(i) for i in comp_mito[:-1]]
+    comp_nucl=subprocess.check_output("cut -f2 "+file+"/composition30/"+file+"-chromosomes.counts.txt", shell=True, universal_newlines=True)
+    comp_nucl=comp_nucl.split('\n')
+    comp_nucl=[int(i) for i in comp_nucl[:-1]]
+
+
+    #Paramter Set III:  Frequency of rN(A,C,G,U) in mitochondria and nucleus
+    freq_mito=subprocess.check_output("cut -f2 "+file+"/composition30/"+file+"-chrM.frequencies.txt", shell=True, universal_newlines=True)
+    freq_mito=freq_mito.split('\n')
+    freq_nucl=subprocess.check_output("cut -f2 "+file+"/composition30/"+file+"-chromosomes.frequencies.txt", shell=True, universal_newlines=True)
+    freq_nucl=freq_nucl.split('\n')
+    
+    #Parameter Set IV: Percentage of counts greater than one in nucleus and mitochondria
+    positions_nucl=subprocess.check_output("grep -v 'chrM' "+file+"/coordinate30/"+file+".counts.tab | wc -l", shell=True, universal_newlines=True)
+    positions_nucl=int(positions_nucl.strip('\n'))
+    positions1_nucl = subprocess.check_output("grep -v 'chrM' "+file+"/coordinate30/"+file+".counts.tab | awk '$7>1' | wc -l", shell=True, universal_newlines=True)
+    positions1_nucl=int(positions1_nucl.strip('\n'))
+    
+    positions_mito=subprocess.check_output("grep 'chrM' "+file+"/coordinate30/"+file+".counts.tab | wc -l", shell=True, universal_newlines=True)
+    positions_mito=int(positions_mito.strip('\n'))
+    positions1_mito= subprocess.check_output("grep 'chrM' "+file+"/coordinate30/"+file+".counts.tab | awk '$7>1' | wc -l", shell=True, universal_newlines=True)
+    positions1_mito=int(positions1_mito.strip('\n'))
+    
+    pos_mito=str(positions1_mito)+"/"+str(positions_mito)
+    pos_nucl=str(positions1_nucl)+"/"+str(positions_nucl)
+    
+    df_row={'Library':file,'Counts_All':total_ribos,'Counts_Nuclear':nucl_total,'Counts_Mito':mito_total,'Comp_nucl_rA':comp_nucl[1]/nucl_total*100,'Comp_nucl_rC':comp_nucl[2]/nucl_total*100,'Comp_nucl_rG':comp_nucl[2]/nucl_total*100,'Comp_nucl_rU':comp_nucl[3]/nucl_total*100,'Freq_nucl_rA':freq_nucl[0],'Freq_nucl_rC':freq_nucl[1],'Freq_nucl_rG':freq_nucl[2],'Freq_nucl_rU':freq_nucl[3],'Comp_mito_rA':comp_mito[0]/mito_total*100,'Comp_mito_rC':comp_mito[1]/mito_total*100,'Comp_mito_rG':comp_mito[2]/mito_total*100,'Comp_mito_rU':comp_mito[3]/mito_total*100,'Freq_mito_rA':freq_mito[0],'Freq_mito_rC':freq_mito[1],'Freq_mito_rG':freq_mito[2],'Freq_mito_rU':freq_mito[3], 'pos>1_nucleus/totpos':pos_nucl, 'pos>1_mito/totpos':pos_mito}
+    new_row=pd.Series(df_row)
+    df=df.append(new_row, ignore_index=True)
+
+df.to_csv('QC.tsv', sep='\t', index=False)
+print("Stats saved in QC.tsv")
+
+
+
+
+
--- qualviz.R
+++ qualviz.R
@@ -1,5 +1,7 @@
 #!/usr/bin/env Rscript
 
+#!/usr/bin/env Rscript
+
 #Taking in arguments
 library("optparse")
  
@@ -46,6 +48,11 @@ library(scales)
 library(stringr) #wrapping Labels
 library(tidyr)
 library(ggpubr)
+if(!require(weatherData)) {install.packages("ggbreak", repos = "http://cran.us.r-project.org")}
+library(ggbreak) 
+library(cowplot)
+library(grid)
+library(gridExtra) 
 
 #2.Defining functions if any
 writeLines("\n...Defining required functions...\n")
@@ -54,18 +61,24 @@ writeLines("\n...Defining required functions...\n")
 writeLines("...Preprocessing input file(s)...")
 QC<-read.table(opt$QC,sep="\t",header=T)
 libinfo<-read.table(opt$libinfo,sep="\t",header=T)
+QC$Library<-libinfo$Label
 
 #4.Main code
 writeLines("\n... Executing Main Code...\n")
 ymax=round(max(QC$Counts_Nuclear/1e6))+1
 perc=QC[,c("Library", "Counts_Nuclear", "Counts_Mito")]
-perc=gather(perc, "Organelle", "counts",-Library)
+groupedcounts=perc
+groupedcounts$Group=libinfo$Group
+groupedcounts$Nuclperc=groupedcounts$Counts_Nuclear/(groupedcounts$Counts_Nuclear+groupedcounts$Counts_Mito)
+groupedcounts$Mitoperc=groupedcounts$Counts_Mito/(groupedcounts$Counts_Nuclear+groupedcounts$Counts_Mito)
+perc=gather(perc, "Genome", "counts",-Library)
+
 
 #5.Plotting
 #writeLines("...Plotting...")
 writeLines("\n... plotting Raw counts...\n")
 
-png("Raw Nuclear counts.png", width=1200, height=700)
+png("Raw Nuclear counts.png", width=12, height=7,units= "in",  res=600)
 ggplot(QC , aes(x=Library, y=Counts_Nuclear/1e6))+
   geom_bar(stat="identity", fill="steelblue")+
   scale_x_discrete(limits=QC$Library,labels = str_replace_all(QC$Library, "-", "\n"))+
@@ -75,10 +88,12 @@ ggplot(QC , aes(x=Library, y=Counts_Nuclear/1e6))+
   ylab("Raw ribonucleotide counts (x10^6)")+
   theme_classic()+
   theme(plot.title = element_text(size=30, face="bold",hjust = 0.5), 
-        axis.text = element_text(size=20, colour = "black"), 
+        axis.text = element_text(size=16, colour = "black"), 
         axis.title = element_text(size=25))
 dev.off()
-png("Raw Mitochondria counts.png", width=1200, height=700)
+
+
+png("Raw Mitochondria counts.png", width=12, height=7,units= "in",  res=600)
 ggplot(QC , aes(x=Library, y=Counts_Mito/1e6))+
   geom_bar(stat="identity", fill="steelblue")+
   scale_x_discrete(limits=QC$Library,labels = str_replace_all(QC$Library, "-", "\n"))+
@@ -88,10 +103,11 @@ ggplot(QC , aes(x=Library, y=Counts_Mito/1e6))+
   ylab("Raw Mitochondria counts (x10^6)")+
   theme_classic()+
   theme(plot.title = element_text(size=30, face="bold",hjust = 0.5), 
-        axis.text = element_text(size=20, colour = "black"), 
+        axis.text = element_text(size=16, colour = "black"), 
         axis.title = element_text(size=25))
 dev.off()
-png("Raw Nuclear counts_horizontal.png", width=1000, height=700)
+
+png("Raw Nuclear counts_horizontal.png", width=12, height=7,units= "in",  res=600)
 ggplot(QC , aes(x=Library, y=Counts_Nuclear/1e6))+
   geom_bar(stat="identity", fill="steelblue")+
   coord_flip()+
@@ -102,10 +118,47 @@ ggplot(QC , aes(x=Library, y=Counts_Nuclear/1e6))+
   ylab("Raw ribonucleotide counts (x10^6)")+
   theme_classic()+
   theme(plot.title = element_text(size=30, face="bold",hjust = 0.5), 
-        axis.text = element_text(size=20, colour = "black"), 
+        axis.text = element_text(size=16, colour = "black"), 
         axis.title = element_text(size=25))
 dev.off()
-png("Raw Mitochondria counts_horizontal.png", width=1000, height=700)
+
+png("Raw Nuclear counts_groupedhorizontal.png", width=18, height=7,units= "in",  res=600, bg="transparent")
+ggplot(groupedcounts , aes(x=Group, y=Counts_Nuclear/1e6))+
+  geom_boxplot(fill="steelblue")+
+  #geom_dotplot(binaxis='y', stackdir='center',position=position_dodge(1))+
+  coord_flip()+
+  scale_x_discrete(limits=rev(unique(groupedcounts$Group)),labels= rev(unique(groupedcounts$Group)))+
+  scale_y_continuous(expand=c(0,0), n.breaks=10, limits = c(0,ymax))+
+  scale_y_break(c(2, 5), scales=1)+
+  ggtitle("Ribonucleotides in Nucleus")+
+  xlab("")+
+  ylab("Raw ribonucleotide counts (x10^6)")+
+  theme_bw(base_size=25)+
+  #theme(plot.title = element_text(size=30, face="bold",hjust = 0.5), 
+        #axis.text = element_text(size=12, colour = "black"), 
+        #axis.title = element_text(size=25))
+	#theme_classic(base_size=20)+
+	theme(legend.title=element_blank(),
+            legend.background = element_rect(fill="transparent"), #element_rect(fill = "transparent",colour = NA),
+            legend.key=element_rect(colour="transparent"), #legend.key = element_rect(fill = "transparent"), 
+            legend.key.size = unit(0.5, "cm"),
+            axis.line = element_line(size=0.2),
+            axis.title=element_blank(),
+            panel.background = element_rect(fill = "transparent",colour = NA),
+            plot.background = element_rect(fill = "transparent",colour = NA),
+            panel.grid.minor = element_blank(), 
+            panel.grid.major = element_blank(),
+            strip.background = element_blank(),
+            plot.margin = unit(c(1, 1, 0.0, 0.0), "cm"), #t, r, b, l
+            axis.text = element_text(color="black"),
+            axis.text.y = element_text(size=25)
+            #axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)
+            )
+dev.off()
+
+
+
+png("Raw Mitochondria counts_horizontal.png", width=12, height=7,units= "in",  res=600)
 ggplot(QC , aes(x=Library, y=Counts_Mito/1e6))+
   geom_bar(stat="identity", fill="steelblue")+
   coord_flip()+
@@ -116,127 +169,98 @@ ggplot(QC , aes(x=Library, y=Counts_Mito/1e6))+
   ylab("Raw Mitochondria counts (x10^6)")+
   theme_classic()+
   theme(plot.title = element_text(size=30, face="bold",hjust = 0.5), 
-        axis.text = element_text(size=20, colour = "black"), 
+        axis.text = element_text(size=16, colour = "black"), 
         axis.title = element_text(size=25))
 dev.off()
 
 
 
 
-writeLines("\n... plotting Percentages...\n")
-png("Nuclear and Mitochondria Percentage.png", width=1200, height=700)
-ggplot(perc, aes(x=Library, y=counts, fill=Organelle))+
+writeLines("\n... plotting Percentages...\n")+
+png("Nuclear and Mitochondria Percentage_legend.png",width=2, height=2,units= "in",  res=600, bg="transparent")
+
+b=ggplot(perc, aes(x=Library, y=counts, fill=Genome))+
   geom_bar(stat="identity", position="fill")+
-  scale_fill_manual(labels = c("Mitochondria", "Nucleus"),values=c("dodgerblue3","firebrick3"))+
+  #scale_fill_manual(labels = c("Mitochondria", "Nucleus"),values=c("dodgerblue3","firebrick3"))+
+  scale_fill_manual(labels = c("Mitochondria", "Nucleus"),values=c("#93A1E5","#E16A86"))+
   scale_x_discrete(limits=QC$Library,labels = str_replace_all(QC$Library, "-", "\n"))+
   scale_y_continuous(expand = c(0,0), labels = scales::percent_format())+
   ggtitle("Nuclear and Mitochondria Distribution")+
   ylab("Percentage")+
   xlab("")+
   theme_classic()+
-  theme(plot.title = element_text(size=25, face="bold",hjust = 0.5), 
-        axis.text = element_text(size=16, colour = "black"),axis.title = element_text(size=20),
-        legend.text = element_text(colour="black", size=16), legend.title = element_text(colour="black", size=16))
+  theme(plot.title = element_text(size=30, face="bold", hjust = 0.5), 
+        axis.text = element_text(size=16, colour = "black"),axis.title = element_text(size=25),
+        legend.text = element_blank(), legend.title = element_blank())
+
+legend<-cowplot::get_legend(b)
+grid.newpage()
+grid.draw(legend)
 dev.off()
-png("Nuclear and Mitochondria Percentage_horizontal.png", width=1000, height=700)
-ggplot(perc, aes(x=Library, y=counts, fill=Organelle))+
-  geom_bar(stat="identity", position="fill")+
+
+
+png("Nuclear and Mitochondria Percentage_horizontal.png", width=12, height=8,units= "in",  res=1000, )
+ggplot(perc, aes(x=Library, y=counts, fill=Genome))+
+  geom_bar(stat="identity", position="fill", width = 0.9)+
+  #geom_text(nudge_y= -.01, color="black",size = 5,fontface="bold")+
   coord_flip()+
-  scale_fill_manual(labels = c("Mitochondria", "Nucleus"),values=c("dodgerblue3","firebrick3"))+
+  scale_fill_manual(labels = c("Mitochondria", "Nucleus"),values=c("#93A1E5","#E16A86"))+#c("#517AC9","#C05D5D"))+
   scale_x_discrete(limits=rev(QC$Library),labels= rev(QC$Library))+
-  scale_y_continuous(expand = c(0,0), labels = scales::percent_format())+
+  scale_y_continuous(expand = c(0,0),labels = scales::percent_format())+
   ggtitle("Nuclear and Mitochondria Distribution")+
   ylab("Percentage")+
   xlab("")+
   theme_classic()+
-  theme(plot.title = element_text(size=25, face="bold",hjust = 0.5), 
-        axis.text = element_text(size=16, colour = "black"),axis.title = element_text(size=20),
-        legend.text = element_text(colour="black", size=16), legend.title = element_text(colour="black", size=16))
+  theme(plot.title = element_text(size=30, face="bold", hjust = 0.5), 
+        axis.text = element_text(size=16, colour = "black"),axis.title = element_text(size=25),
+        plot.margin = unit(c(1, 1, 0.0, 0.0), "cm"), #t, r, b, l
+        legend.text = element_blank(), legend.title = element_blank()) + 
+  theme(legend.position = "none")
+dev.off()
+
+png("Nuclear percent_groupedhorizontal.png", width=12, height=7,units= "in",  res=600)
+ggplot(groupedcounts , aes(x=Group, y=Nuclperc*100))+
+  geom_boxplot(fill="#E16A86")+
+  geom_point()+
+  coord_flip()+
+  scale_x_discrete(limits=rev(unique(groupedcounts$Group)),labels= rev(unique(groupedcounts$Group)))+
+  scale_y_continuous(expand=c(0,0), n.breaks=10, limits = c(0,120))+
+  #scale_y_break(c(80, 90), scales=2)+
+  ggtitle("Ribonucleotide percentage in Nucleus")+
+  xlab("")+
+  ylab("Percentage")+
+  theme_classic()+
+  theme(plot.title = element_text(size=30, face="bold",hjust = 0.5), 
+        axis.text = element_text(size=12, colour = "black"), 
+        axis.title = element_text(size=25))
 dev.off()
 
 
 writeLines("\n... plotting composition...\n")
-png("Nuclear Grouped Composition.png", width=1200, height=700)
-df=QC[,grep(pattern="Comp_nucl", x=colnames(QC))]
-df$Group=libinfo$Group
-data=gather(df,key="rNTP", value = "Percentage", -Group)
-obj<-ggbarplot(data, x = "Group", y = "Percentage", fill="rNTP",
-               add = c("mean_sd", "point"),
-               #error.plot=c("crossbar"),
-               add.params=list(width=0.4),
-               palette = c("#D55E00","#0072B2", "#F0E442","#009E73"),
-               position = position_dodge(0.75), 
-               xlab="", ylab="Percentage", 
-               width = 0.75,
-               size = 1) +
-  scale_y_continuous(expand = c(0,0),limits = c(0,100), breaks = seq(0,100,10)) +
-  theme_classic(base_size = 30)+
-  scale_x_discrete(labels = function(x) str_wrap(x, width = 12))
-ggpar(obj, legend=c("right"), legend.title = "")
 
-dev.off()
+for (comp in c("Comp_mito", "Comp_nucl", "Freq_mito", "Freq_nucl")) {
+  png(paste(comp,".png",sep=""),width=10, height=6,units= "in",  res=600)
+  df=QC[,grep(pattern=comp, x=colnames(QC))]
+  df$Group=libinfo$Group
+  data=gather(df,key="rNTP", value = "Percentage", -Group)
+  obj<-ggbarplot(data, x = "Group", y = "Percentage", fill="rNTP",
+                add = c("mean_se"),
+                add.params=list(width=0.4,size=0.5),
+                palette = c("#D55E00","#0072B2", "#F0E442","#009E73"),
+                position = position_dodge(0.75), 
+                xlab="", ylab="Percentage", 
+                width = 0.75,
+                size = 0.5) +
+    geom_point(data,mapping=aes(Group,Percentage,color=rNTP), position=position_dodge(0.75),size=1)+guides(color = FALSE)+
+    scale_color_manual(values=c("black","black","black","black"))+
+    scale_y_continuous(expand = c(0,0),limits = c(0,100), breaks = seq(0,100,10)) +
+    theme_classic(base_size = 20)+
+    scale_x_discrete(labels = function(x) str_wrap(x, width = 12))
+  p=ggpar(obj, legend=c("right"), legend.title = "")
+  print(p)
+  dev.off()
 
-png("Mitochondria Grouped Composition.png", width=1200, height=700)
-df=QC[,grep(pattern="Comp_mito", x=colnames(QC))]
-df$Group=libinfo$Group
-data=gather(df,key="rNTP", value = "Percentage", -Group)
-obj<-ggbarplot(data, x = "Group", y = "Percentage", fill="rNTP",
-               add = c("mean_sd", "point"),
-               #error.plot=c("crossbar"),
-               add.params=list(width=0.4),
-               palette = c("#D55E00","#0072B2", "#F0E442","#009E73"),
-               position = position_dodge(0.75), 
-               xlab="", ylab="Percentage", 
-               width = 0.75,
-               size = 1) +
-  scale_y_continuous(expand = c(0,0),limits = c(0,100), breaks = seq(0,100,10)) +
-  theme_classic(base_size = 30)+
-  scale_x_discrete(labels = function(x) str_wrap(x, width = 12))
-ggpar(obj, legend=c("right"), legend.title = "")
-
-dev.off()
-png("Nuclear Grouped Normalized Composition.png", width=1200, height=700)
-df=QC[,grep(pattern="Freq_nucl", x=colnames(QC))]
-df$Group=libinfo$Group
-data=gather(df,key="rNTP", value = "Percentage", -Group)
-obj<-ggbarplot(data, x = "Group", y = "Percentage", fill="rNTP",
-               add = c("mean_sd", "point"),
-               #error.plot=c("crossbar"),
-               add.params=list(width=0.4),
-               palette = c("#D55E00","#0072B2", "#F0E442","#009E73"),
-               position = position_dodge(0.75), 
-               xlab="", ylab="Normalized Percentage", 
-               width = 0.75,
-               size = 1) +
-  scale_y_continuous(expand = c(0,0),limits = c(0,100), breaks = seq(0,100,10)) +
-  theme_classic(base_size = 30)+
-  scale_x_discrete(labels = function(x) str_wrap(x, width = 12))
-ggpar(obj, legend=c("right"), legend.title = "")
-
-dev.off()
-
-png("Mitochondria Grouped  Normalized Composition.png", width=1200, height=700)
-df=QC[,grep(pattern="Freq_mito", x=colnames(QC))]
-df$Group=libinfo$Group
-data=gather(df,key="rNTP", value = "Percentage", -Group)
-obj<-ggbarplot(data, x = "Group", y = "Percentage", fill="rNTP",
-               add = c("mean_sd", "point"),
-               #error.plot=c("crossbar"),
-               add.params=list(width=0.4),
-               palette = c("#D55E00","#0072B2", "#F0E442","#009E73"),
-               position = position_dodge(0.75), 
-               xlab="", ylab="Normalized Percentage", 
-               width = 0.75,
-               size = 1) +
-  scale_y_continuous(expand = c(0,0),limits = c(0,100), breaks = seq(0,100,10)) +
-  theme_classic(base_size = 30)+
-  scale_x_discrete(labels = function(x) str_wrap(x, width = 12))
-ggpar(obj, legend=c("right"), legend.title = "")
-
-dev.off()
-
-
-
+}
 
 writeLines("\nJob Finished.\n")
-writeLines(paste("Output files in current folder")
+writeLines(paste("Output files in current folder"))
